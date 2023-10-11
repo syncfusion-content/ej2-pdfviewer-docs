e452fd2b08b521aa1c3de64dc3fd707354c458f1
SaravanaPriya31 <Saravana31>
2023-10-11 15:53:06 +0530

    commit

--- ej2-javascript/code-snippet/pdfviewer/addformfield-cs1/systemjs.config.js
+++ ej2-javascript/code-snippet/pdfviewer/addformfield-cs1/systemjs.config.js
@@ -16,7 +16,7 @@ System.config({
         main: "index.ts",
         typescript: "https://unpkg.com/typescript@2.2.2/lib/typescript.js",
         'plugin-json':'https://cdnjs.cloudflare.com/ajax/libs/systemjs-plugin-json/0.3.0/json.min.js',
-                "@syncfusion/ej2-base": "syncfusion:ej2-base/dist/ej2-base.umd.min.js",
+        "@syncfusion/ej2-base": "syncfusion:ej2-base/dist/ej2-base.umd.min.js",
         "@syncfusion/ej2-buttons": "syncfusion:ej2-buttons/dist/ej2-buttons.umd.min.js",
         "@syncfusion/ej2-popups": "syncfusion:ej2-popups/dist/ej2-popups.umd.min.js",
         "@syncfusion/ej2-navigations": "syncfusion:ej2-navigations/dist/ej2-navigations.umd.min.js",
