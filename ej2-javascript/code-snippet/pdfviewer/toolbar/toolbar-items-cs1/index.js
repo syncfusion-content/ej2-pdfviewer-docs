c9d88fe0795ab07831334dc5613f21e41b36e470
SaravanaPriya31 <Saravana31>
2023-10-18 17:10:20 +0530

    toolbar commit

--- ej2-javascript/code-snippet/pdfviewer/toolbar/toolbar-items-cs1/index.js
+++ ej2-javascript/code-snippet/pdfviewer/toolbar/toolbar-items-cs1/index.js
@@ -1,7 +1,7 @@
 let pdfviewer: PdfViewer = new PdfViewer({
     documentPath:'https://cdn.syncfusion.com/content/pdf/pdf-succinctly.pdf',
     resourceUrl:"https://cdn.syncfusion.com/ej2/23.1.40/dist/ej2-pdfviewer-lib",
-    toolbarSettings: { showTooltip : true, toolbarItem: ['OpenOption']}
+    toolbarSettings: { showTooltip : true, toolbarItems: ['OpenOption']}
 });
 pdfviewer.appendTo('#PdfViewer');
 
--- ej2-javascript/code-snippet/pdfviewer/toolbar/toolbar-items-cs1/index.ts
+++ ej2-javascript/code-snippet/pdfviewer/toolbar/toolbar-items-cs1/index.ts
@@ -7,7 +7,7 @@ PdfViewer.Inject( Toolbar, Magnification, Navigation, Annotation, LinkAnnotation
 let pdfviewer: PdfViewer = new PdfViewer({
     documentPath:'https://cdn.syncfusion.com/content/pdf/pdf-succinctly.pdf',
     resourceUrl:"https://cdn.syncfusion.com/ej2/23.1.40/dist/ej2-pdfviewer-lib",
-    toolbarSettings: { showTooltip : true, toolbarItem: ['OpenOption']}
+    toolbarSettings: { showTooltip : true, toolbarItems: ['OpenOption']}
 });
 pdfviewer.appendTo('#PdfViewer');
 
