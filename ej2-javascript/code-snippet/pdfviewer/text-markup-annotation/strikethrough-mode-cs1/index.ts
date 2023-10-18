a7a48550d0ea58b6cf87fb326c8738e9b3ccc1fd
SaravanaPriya31 <Saravana31>
2023-10-18 14:46:25 +0530

    commit

--- ej2-javascript/code-snippet/pdfviewer/text-markup-annotation/strikethrough-mode-cs1/index.ts
+++ ej2-javascript/code-snippet/pdfviewer/text-markup-annotation/strikethrough-mode-cs1/index.ts
@@ -6,8 +6,9 @@ import { PdfViewer, Toolbar, Magnification, Navigation, LinkAnnotation,Thumbnail
 PdfViewer.Inject( Toolbar,Magnification, Navigation, LinkAnnotation,ThumbnailView,
                   BookmarkView, TextSelection, Annotation, FormDesigner, FormFields);
 
-let pdfviewer: PdfViewer = new PdfViewer({ documentPath:'https://cdn.syncfusion.com/content/pdf/pdf-succinctly.pdf' });
-pdfviewer.serviceUrl = 'https://services.syncfusion.com/js/production/api/pdfviewer';
+let pdfviewer: PdfViewer = new PdfViewer({ 
+documentPath:'https://cdn.syncfusion.com/content/pdf/pdf-succinctly.pdf' 
+});
 pdfviewer.resourceUrl ="https://cdn.syncfusion.com/ej2/23.1.40/dist/ej2-pdfviewer-lib";
 pdfviewer.appendTo('#PdfViewer');
 
