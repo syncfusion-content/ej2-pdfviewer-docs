d4ffc1837cdd9d3f93be70b137b77c2a80c00acf
SaravanaPriya31 <Saravana31>
2023-10-18 19:24:58 +0530

    toolbar commit

--- ej2-javascript/code-snippet/pdfviewer/toolbar/toolbar-items-cs1/index.js
+++ ej2-javascript/code-snippet/pdfviewer/toolbar/toolbar-items-cs1/index.js
@@ -1,6 +1,7 @@
 let pdfviewer: PdfViewer = new PdfViewer({
     documentPath:'https://cdn.syncfusion.com/content/pdf/pdf-succinctly.pdf',
     resourceUrl:"https://cdn.syncfusion.com/ej2/23.1.40/dist/ej2-pdfviewer-lib",
+    serviceUrl: 'https://services.syncfusion.com/js/production/api/pdfviewer',
     toolbarSettings: { showTooltip : true, toolbarItems: ['OpenOption']}
 });
 pdfviewer.appendTo('#PdfViewer');
--- ej2-javascript/code-snippet/pdfviewer/toolbar/toolbar-method-cs1/index.ts
+++ ej2-javascript/code-snippet/pdfviewer/toolbar/toolbar-method-cs1/index.ts
@@ -7,7 +7,8 @@ PdfViewer.Inject( Toolbar, Magnification, Navigation, Annotation, LinkAnnotation
 
 let pdfviewer: PdfViewer = new PdfViewer({
     documentPath:'https://cdn.syncfusion.com/content/pdf/pdf-succinctly.pdf',
-    resourceUrl:"https://cdn.syncfusion.com/ej2/23.1.40/dist/ej2-pdfviewer-lib"
+    resourceUrl:"https://cdn.syncfusion.com/ej2/23.1.40/dist/ej2-pdfviewer-lib",
+    serviceUrl: 'https://services.syncfusion.com/js/production/api/pdfviewer'
 });
 pdfviewer.appendTo('#PdfViewer');
 document.getElementById('set').addEventListener('click', ()=> {
