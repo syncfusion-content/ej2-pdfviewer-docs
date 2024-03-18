27106a2a3c3595fd53d46ac7018eb5019b7741de
SaravanaPriya31 <Saravana31>
2024-03-18 09:34:48 +0530

    872504: commit

--- ej2-javascript/code-snippet/pdfviewer/custom-context-menu/index.js
+++ ej2-javascript/code-snippet/pdfviewer/custom-context-menu/index.js
@@ -1,14 +1,15 @@
 var pdfviewer = new ej.pdfviewer.PdfViewer({
-    documentPath: 'https://cdn.syncfusion.com/content/pdf/form-designer.pdf',
-    resourceUrl: 'https://cdn.syncfusion.com/ej2/24.1.41/dist/ej2-pdfviewer-lib'
+    documentPath: 'https://cdn.syncfusion.com/content/pdf/pdf-succinctly.pdf',
+    resourceUrl:'https://cdn.syncfusion.com/ej2/24.2.3/dist/ej2-pdfviewer-lib'
 });
 ej.pdfviewer.PdfViewer.Inject(ej.pdfviewer.TextSelection, ej.pdfviewer.TextSearch, ej.pdfviewer.Print, ej.pdfviewer.Navigation, ej.pdfviewer.Toolbar,
-    ej.pdfviewer.Magnification, ej.pdfviewer.Annotation, ej.pdfviewer.FormDesigner, ej.pdfviewer.FormFields);
+                              ej.pdfviewer.Magnification, ej.pdfviewer.Annotation, ej.pdfviewer.FormDesigner, ej.pdfviewer.FormFields, ej.pdfviewer.PageOrganizer);
+
 var menuItems = [
     {
         text: 'Search In Google',
         id: 'search_in_google',
-        iconCss: 'e-icons e-de-ctnr-find'
+        iconCss: 'e-icons e-search'
     },
     {
         text: 'Lock Annotation',
@@ -86,9 +87,14 @@ pdfviewer.customContextMenuBeforeOpen = function (args) {
                 }
             } else if ((args.ids[i] === "read_only_true" || args.ids[i] === "read_only_false") && pdfviewer.selectedItems.formFields.length !== 0) {
                 var isReadOnlyOption = args.ids[i] === "read_only_true";
-                var selectedFormField = pdfviewer.selectedItems.formFields[0].isReadonly;
-                var displayMenu = (isReadOnlyOption && !selectedFormField) || (!isReadOnlyOption && selectedFormField);
-                search.style.display = displayMenu ? 'block' : 'none';
+                for (var k = 0; k < pdfviewer.selectedItems.formFields.length; k++) {
+                    var selectedFormFields = pdfviewer.selectedItems.formFields[k];
+                    if (selectedFormFields) {
+                        var selectedFormField = pdfviewer.selectedItems.formFields[k].isReadonly;
+                        var displayMenu = (isReadOnlyOption && !selectedFormField) || (!isReadOnlyOption && selectedFormField);
+                        search.style.display = displayMenu ? 'block' : 'none';
+                    }
+                }
             } else if (args.ids[i] === 'formfield properties' && pdfviewer.selectedItems.formFields.length !== 0) {
                 search.style.display = 'block';
             }
@@ -97,26 +103,24 @@ pdfviewer.customContextMenuBeforeOpen = function (args) {
 };
 
 function lockAnnotations(args) {
-    var selectedAnnotations = pdfviewer.selectedItems.annotations;
-    for (var i = 0; i < selectedAnnotations.length; i++) {
-        var annotation = selectedAnnotations[i];
-        if (annotation && annotation.annotationSettings) {
-            annotation.annotationSettings.isLock = true;
-            pdfviewer.annotationModule.editAnnotation(annotation);
-            args.cancel = false;
+    for (var i = 0; i < pdfviewer.annotationCollection.length; i++) {
+        if (pdfviewer.annotationCollection[i].uniqueKey === pdfviewer.selectedItems.annotations[0].id) {
+            pdfviewer.annotationCollection[i].annotationSettings.isLock = true;
+            pdfviewer.annotationCollection[i].isCommentLock = true;
+            pdfviewer.annotation.editAnnotation(pdfviewer.annotationCollection[i]);
         }
+        args.cancel = false;
     }
 }
 
 function unlockAnnotations(args) {
-    var selectedAnnotations = pdfviewer.selectedItems.annotations;
-    for (var i = 0; i < selectedAnnotations.length; i++) {
-        var annotation = selectedAnnotations[i];
-        if (annotation && annotation.annotationSettings) {
-            annotation.annotationSettings.isLock = false;
-            pdfviewer.annotationModule.editAnnotation(annotation);
-            args.cancel = false;
+    for (var i = 0; i < pdfviewer.annotationCollection.length; i++) {
+        if (pdfviewer.annotationCollection[i].uniqueKey === pdfviewer.selectedItems.annotations[0].id) {
+            pdfviewer.annotationCollection[i].annotationSettings.isLock = false;
+            pdfviewer.annotationCollection[i].isCommentLock = false;
+            pdfviewer.annotation.editAnnotation(pdfviewer.annotationCollection[i]);
         }
+        args.cancel = false;
     }
 }
 
@@ -148,15 +152,16 @@ function setReadOnlyFalse(args) {
 
 var defaultCheckBoxObj = new ej.buttons.CheckBox({
     change: contextmenuHelper,
-    cssClass: 'multiline',
+    label: "Hide Default Context Menu"
 });
-defaultCheckBoxObj.appendTo('#hide-default-context-menu');
+defaultCheckBoxObj.appendTo('#hide');
 
 var positionCheckBoxObj = new ej.buttons.CheckBox({
     change: contextmenuHelper,
-    cssClass: 'multiline',
+    label: "Add Custom option at bottom"
+    
 });
-positionCheckBoxObj.appendTo('#show-custom-menu-bottom');
+positionCheckBoxObj.appendTo('#toolbar');
 
 function contextmenuHelper(args) {
     pdfviewer.addCustomMenu(menuItems, defaultCheckBoxObj.checked, positionCheckBoxObj.checked);
